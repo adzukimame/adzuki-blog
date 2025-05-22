9d073815200fe5d3ced5e56d8ba84c0a66a527bf
adzukimame <88547072+adzukimame@users.noreply.github.com>
2025-05-23 00:55:45 +0900

    リンクカードを含むページをSSRでレンダリングすると、常にURLプレビューが失敗した表示になるのを修正 (#26)
    * リンクカードを含むページをSSRでレンダリングすると常にURLプレビューが失敗した表示になるのを修正
    
    * fix comments
    
    ---------
    
    Co-authored-by: adzukimame <88547072+adzuki02@users.noreply.github.com>

--- components/content/LinkCard.vue
+++ components/content/LinkCard.vue
@@ -59,7 +59,10 @@ useFetch<SummalyResult>(
     server: false,
   }
 ).then(({ data: result }) => {
-  data.value = result.value;
+  // server: falseにすると、サーバーで必ずdataがnullになるので、クライアントの場合のみ代入する
+  if (import.meta.client) {
+    data.value = result.value;
+  }
 });
 </script>
 
