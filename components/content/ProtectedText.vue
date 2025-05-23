4e534ceb5bf0977c8d0e2375b795d5c461723bbe
adzukimame <88547072+adzuki02@users.noreply.github.com>
2025-05-24 01:29:10 +0900

    wip

--- components/content/ProtectedText.vue
+++ components/content/ProtectedText.vue
@@ -1,40 +1,55 @@
 <template>
   <ClientOnly>
-    <div ref="textBlock">
-      <!--  -->
+    <div v-if="loading">
+      Loading...
     </div>
+    <div v-else-if="error">
+      An error occured during rendering.
+    </div>
+    <div v-else
+         ref="textBlock" />
     <template #fallback />
   </ClientOnly>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   name: string;
-}>();
+  fontSizeInRem?: number;
+}>(), {
+  fontSizeInRem: 1,
+});
 
+const loading = ref(true);
+const error = ref(false);
 const textBlock = useTemplateRef('textBlock');
 
 const renderCanvas = (text: string) => {
   if (textBlock === null) {
+    error.value = true;
     return;
   }
+
+  const rootFontSizeInPx = parseFloat(getComputedStyle(document.documentElement).fontSize);
+  const fontSize = rootFontSizeInPx * props.fontSizeInRem;
 
   for (let i = 0; i < text.length; i++) {
     const canvas = document.createElement('canvas');
     canvas.style.pointerEvents = 'none';
     canvas.addEventListener('contextmenu', ev => ev.preventDefault());
-    canvas.width = 24;
-    canvas.height = 28;
+    canvas.width = fontSize;
+    canvas.height = fontSize + 4;
     textBlock.value?.appendChild(canvas);
 
     const ctx = canvas.getContext('2d');
     if (ctx === null) {
+      error.value = true;
       return;
     }
-    ctx.font = '24px sans-serif';
+    ctx.font = `${fontSize}px "Zen Maru Gothic"`;
     ctx.textBaseline = 'middle';
     ctx.textAlign = 'center';
-    ctx.fillText(text[i], 12, 14);
+    ctx.fillText(text[i], fontSize / 2, fontSize / 2 + 2);
   }
 };
 
@@ -45,18 +60,22 @@ if (import.meta.client) {
     },
     watch: [() => props.name],
   }).then((response) => {
+    loading.value = false;
+
     const header = response.headers.get('X-Attached-Payload');
     if (header === null) {
+      error.value = true;
       return;
     }
     const rand = Uint8Array.from(atob(header), char => char.codePointAt(0)!);
 
     const data = response._data;
-    if (data == null) {
+    if (!(data instanceof Blob)) {
+      error.value = true;
       return;
     }
 
-    data.arrayBuffer().then(buffer => {
+    data.arrayBuffer().then((buffer) => {
       const byteArray = new Uint8Array(buffer);
       const text = new TextDecoder().decode(byteArray.map((byte, idx) => byte ^ rand[idx]));
       renderCanvas(text);
@@ -64,69 +83,3 @@ if (import.meta.client) {
   });
 }
 </script>
-
-<style module>
-.container {
-  display: block grid;
-  grid-template-rows: 2rem 1.6rem 1.6rem;
-  padding-inline-start: 1rem;
-  background-color: var(--bgStrong);
-  border: solid 1px var(--split);
-  border-radius: 6px;
-  line-height: 2;
-  transition: background-color var(--colorSchemeTransitionDuration);
-}
-
-.title {
-  display: block;
-  font-size: 1rem;
-  color: var(--fg);
-  overflow: clip;
-  transition: text-decoration var(--hoverTransitionDuration) var(--hoverTransitionFunction);
-  transition: color var(--hoverTransitionDuration) var(--hoverTransitionFunction);
-}
-
-.title:not(.loaded) {
-  cursor: default;
-}
-
-@media (hover: hover) {
-  .container:hover>.title.loaded {
-    color: var(--fgStrong);
-    text-decoration: underline;
-  }
-}
-
-@media (hover: none) {
-  .container:active>.title.loaded {
-    color: var(--fgStrong);
-    text-decoration: underline;
-  }
-}
-
-.description {
-  display: block;
-  font-size: 0.8rem;
-  color: var(--fgWeak);
-  overflow: clip;
-}
-
-.faviconAndHostnameContainer {
-  display: block flex;
-  font-size: 0.8rem;
-  align-items: center;
-}
-
-.favicon {
-  display: block;
-  inline-size: 0.8rem;
-  block-size: 0.8rem;
-  object-fit: contain;
-}
-
-.hostname {
-  color: var(--fg);
-  overflow: clip;
-  padding-inline-start: 0.5rem;
-}
-</style>
