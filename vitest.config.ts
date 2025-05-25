5fb3d4ddb7a3a43167d5bca54ce202f0e26ca715
adzukimame <88547072+adzuki02@users.noreply.github.com>
2025-05-25 20:04:34 +0900

    add coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,16 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config';
+import { coverageConfigDefaults } from 'vitest/config';
 
 export default defineVitestConfig({
   test: {
     environment: 'nuxt',
+    coverage: {
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        'nuxt.config.ts',
+        'app.config.ts',
+        'scripts',
+      ],
+    },
   },
 });
