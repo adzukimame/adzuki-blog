5a19fbef6c5547146eb7142255b612afb62cd654
adzukimame <88547072+adzukimame@users.noreply.github.com>
2025-05-28 19:35:40 +0900

    すべてのcontentの内容を含むJSONファイルをバンドルしない (#51)

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -1,5 +1,9 @@
 // @ts-check
-import { copyFileSync } from 'node:fs';
+import { copyFileSync, rmSync, globSync } from 'node:fs';
 
 // _headers
 copyFileSync('./_headers', './dist/_headers');
+
+globSync('./dist/api/_content/cache.*.json').forEach((path) => {
+  rmSync(path, { force: true });
+});
