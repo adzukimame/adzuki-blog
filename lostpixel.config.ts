f9778f05c41983fea6aab93db5a74fdfbf61d354
adzukimame <88547072+adzuki02@users.noreply.github.com>
2025-05-30 17:27:04 +0900

    wip

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,48 +1,75 @@
 import { readFile } from 'node:fs/promises';
 import { launchStaticWebServer } from 'lost-pixel/dist/crawler/utils.js';
+import { z } from 'zod/v4';
 import sanitizeFilename from 'sanitize-filename';
 import type { CustomProjectConfig } from 'lost-pixel';
+import type { getSerializedStoryData } from 'histoire/src/node/build-serialize.js';
+
+const interactSchema = z.array(z.array(z.union([
+  z.object({ click: z.string() }),
+  z.object({ hover: z.string() }),
+  z.object({ sleep: z.number() }),
+])));
+type Interact = z.infer<typeof interactSchema>;
+
+const customPages: NonNullable<CustomProjectConfig['pageShots']>['pages']
+  = await readFile('./.histoire/dist/histoire.json', { encoding: 'utf8' })
+    .then(text => JSON.parse(text))
+    .then((histoire: ReturnType<typeof getSerializedStoryData>) =>
+      histoire.stories.map((story) => {
+        const storyInteract = story.meta && 'interact' in story.meta && Array.isArray(story.meta.interact) ? story.meta.interact as unknown[] : undefined;
+
+        return story.variants.map((variant) => {
+          const variantInteract = variant.meta && 'interact' in variant.meta && Array.isArray(variant.meta.interact) ? variant.meta.interact as unknown[] : undefined;
+
+          if (storyInteract !== undefined && variantInteract !== undefined) {
+            return {
+              storyId: story.id,
+              variantId: variant.id,
+              variantTitle: variant.title,
+              interact: [
+                ...(storyInteract.filter(i => Array.isArray(i)) as unknown[][]),
+                ...(variantInteract.filter(i => Array.isArray(i)) as unknown[][]),
+              ],
+            };
+          }
+          else {
+            return undefined;
+          }
+        });
+      }).flat()
+        .filter(variant => variant !== undefined)
+        .map((variant) => {
+          return variant.interact.map((i) => {
+            if (i.every(op => op !== null && typeof op === 'object')) {
+              return {
+                storyId: variant.storyId,
+                variantId: variant.variantId,
+                variantTitle: variant.variantTitle,
+                interact: i,
+              };
+            }
+            else {
+              return undefined;
+            }
+          });
+        }).flat()
+        .filter(variant => variant !== undefined)
+        .map((variant) => {
+          const url = new URL(server.url);
+          url.pathname = '/__sandbox.html';
+          url.searchParams.set('storyId', variant.storyId);
+          url.searchParams.set('variantId', variant.variantId);
+          url.searchParams.set('interact', encodeURIComponent(JSON.stringify(variant.interact)));
+
+          return {
+            path: url.pathname + url.search,
+            name: sanitizeFilename(`${variant.storyId}_${variant.variantTitle}_${variant.interact.map(op => Object.entries(op).map(pair => pair.join('-')).join('--')).join('---')}`),
+          };
+        })
+    );
 
 const server = await launchStaticWebServer('.histoire/dist');
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-const customPages: NonNullable<CustomProjectConfig['pageShots']>['pages'] = await readFile('./.histoire/dist/histoire.json', { encoding: 'utf8' }).then(text => JSON.parse(text)).then((histoire) => {
-  return histoire.stories.map((story: any) => {
-    const storyInteract = 'meta' in story && story.meta !== null && typeof story.meta === 'object' && 'interact' in story.meta && Array.isArray(story.meta.interact) ? story.meta?.interact : undefined;
-
-    return story.variants.map((variant: any) => {
-      const variantInteract = 'meta' in variant ? variant.meta?.interact : undefined;
-
-      if (storyInteract === undefined && variantInteract === undefined) return undefined;
-
-      return {
-        storyId: story.id,
-        variantId: variant.id,
-        variantTitle: variant.title,
-        interact: [...(storyInteract ?? []), ...(variantInteract ?? [])] as NonNullable<typeof variantInteract>,
-      };
-    });
-  }).flat().filter((variant: any) => variant !== undefined).map((variant: any) => {
-    return variant.interact.map((i: any) => ({
-      storyId: variant.storyId,
-      variantId: variant.variantId,
-      variantTitle: variant.variantTitle,
-      interact: i,
-    }));
-  }).flat().map((variant: any) => {
-    const url = new URL(server.url);
-    url.pathname = '/__sandbox.html';
-    url.searchParams.set('storyId', variant.storyId);
-    url.searchParams.set('variantId', variant.variantId);
-    url.searchParams.set('interact', encodeURIComponent(JSON.stringify(variant.interact)));
-
-    return {
-      path: url.pathname + url.search,
-      name: sanitizeFilename(`${variant.storyId}_${variant.variantTitle}_${variant.interact.map((op: any) => Object.entries(op).map(pair => pair.join('-')).join('--')).join('---')}`),
-    };
-  });
-});
 
 export const config: CustomProjectConfig = {
   histoireShots: {
@@ -56,17 +83,15 @@ export const config: CustomProjectConfig = {
     const url = new URL(page.url());
 
     if (url.searchParams.has('interact')) {
-      const interact: any[] = JSON.parse(decodeURIComponent(url.searchParams.get('interact')!));
+      const interact: object[] = JSON.parse(decodeURIComponent(url.searchParams.get('interact')!));
 
       for (const i of interact) {
-        if (i.click) await page.click(i.click);
-        else if (i.hover) await page.locator(i.hover).hover();
-        else if (i.sleep) await new Promise<void>(res => setTimeout(res, parseInt(i.sleep)));
+        if ('click' in i && typeof i.click === 'string') await page.click(i.click);
+        else if ('hover' in i && typeof i.hover === 'string') await page.locator(i.hover).hover();
+        else if ('sleep' in i && typeof i.sleep === 'number') await new Promise<void>(res => setTimeout(res, i.sleep as number));
       }
     }
   },
   generateOnly: true,
   failOnDifference: process.env.LOST_PIXEL_MODE !== 'update',
 };
-
-/* eslint-enable @typescript-eslint/no-explicit-any */
