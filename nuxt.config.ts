41d5ad65ff274a7467c3d9c522ebf3ec7ece1d47
adzukimame <88547072+adzuki02@users.noreply.github.com>
2025-07-29 12:10:15 +0900

    Histoireのスタイルも変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -88,7 +88,7 @@ export default defineNuxtConfig({
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-        { href: 'https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400&display=swap', rel: 'stylesheet' },
+        { href: 'https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap', rel: 'stylesheet' },
       ],
     },
   },
