1adb395d39bbe8e202ab77e0f7e78fab83ad4cbd
adzukimame <88547072+adzuki02@users.noreply.github.com>
2025-07-29 14:08:27 +0900

    fix histoire

--- histoire/GlobalWrapper.vue
+++ histoire/GlobalWrapper.vue
@@ -30,14 +30,15 @@ classObserver.observe(document.documentElement, { attributes: true, attributeFil
 </script>
 
 <style>
-/* @import url('https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap'); */
+/* 日本語対応の明朝体がインストールされていないことがあるのでWebフォントを読み込んでおく */
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap');
 </style>
 
 <style scoped>
 .outer {
   color: var(--fg);
   background-color: var(--bg);
-  font-family: /* "Noto Serif JP",  */"Yu Mincho", "Hiragino Mincho ProN W3", serif;
+  font-family: "Noto Serif JP", "Yu Mincho", "Hiragino Mincho ProN W3", serif;
   font-feature-settings: "pkna" 1;
 }
 
