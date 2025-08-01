80f64196181298ca7114608f9e840aa046860c31
adzukimame <88547072+adzuki02@users.noreply.github.com>
2025-08-02 00:44:42 +0900

    organized nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,29 +18,25 @@ const cyrb53 = (str: string, seed = 0) => {
   return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 };
 
-// https://github.com/jyn514/base56/blob/master/base56.py
 const ALPHABET = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz';
-function encodeNumber(number: number): string {
-  if (number === 0) {
-    return '0';
-  }
+const extractLowerBits = (number: number, digits: number) => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  if (number === 0) return ALPHABET[0]!.repeat(digits);
 
   let result = '';
-
-  while (number > 0) {
-    const index = number % ALPHABET.length;
+  for (let i = 0; i < digits && number > 0; i++) {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    result = ALPHABET[index]! + result;
+    result += ALPHABET[number % ALPHABET.length]!;
     number = Math.floor(number / ALPHABET.length);
   }
-
-  return result;
-}
+  return result.padStart(digits, ALPHABET[0]);
+};
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-24',
   srcDir: 'src/',
   devtools: { enabled: true },
+  telemetry: false,
   modules: [
     '@nuxt/content',
     '@nuxt/image',
@@ -120,6 +116,15 @@ export default defineNuxtConfig({
     prerender: {
       autoSubfolderIndex: false,
     },
+    hooks: {
+      'prerender:generate': (route) => {
+        // https://nuxt.com/docs/3.x/getting-started/prerendering#prerendergenerate-nitro-hook
+        if (/^\/api\/_content\/cache\.\d+\.json$/.exec(route.route)) {
+          route.skip = true;
+        }
+      },
+    },
+    sourceMap: false,
   },
   vite: {
     css: {
@@ -128,13 +133,26 @@ export default defineNuxtConfig({
           const id = `${new URL(filename, import.meta.url).pathname.replace(new URL('./', import.meta.url).pathname, '')}-${name}`.replace(/[\\/.?&=]/g, '-');
 
           if (process.env.NODE_ENV === 'production') {
-            return encodeNumber(cyrb53(id)).substring(0, 5);
+            return extractLowerBits(cyrb53(id), 5);
           }
           else {
             return id;
           }
         },
       },
+    },
+  },
+  hooks: {
+    'build:manifest': (manifest) => {
+      // https://nuxt.com/docs/3.x/getting-started/styling#lcp-advanced-optimizations
+      const css = Object.values(manifest).find(options => options.isEntry)?.css;
+      if (css) {
+        for (let i = css.length - 1; i >= 0; i--) {
+          if (css[i]?.startsWith('entry.')) {
+            css.splice(i, 1);
+          }
+        }
+      }
     },
   },
   $production: {
