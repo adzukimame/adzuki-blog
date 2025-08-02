3dc60b957815de0ac0cc56dba32a8fed97fc36f6
adzukimame <88547072+adzuki02@users.noreply.github.com>
2025-08-02 23:14:47 +0900

    update baseline

--- scripts/interpret-lost-pixel-result.js
+++ scripts/interpret-lost-pixel-result.js
@@ -118,7 +118,7 @@ export const interpretAndMarkup = () => {
     markup += `❌${differingStories.length} stories/variants have differences.
 |Story Title|Variant Title|Story Path|Dark Mode|Writing Mode|Interaction|
 |-----------|-------------|----------|---------|------------|-----------|
-${differingStories.map(storyInfo => `|${storyInfo.title}|${storyInfo.variantTitle}|${storyInfo.relativePath}|${storyInfo.darkMode}|${storyInfo.writingMode ?? ''}|${storyInfo.interact ?? ''}|`).join('\n')}\n\n`;
+${differingStories.map(storyInfo => `|${storyInfo.title}|${storyInfo.variantTitle}|${storyInfo.relativePath}|${storyInfo.darkMode || ''}|${storyInfo.writingMode ?? ''}|${storyInfo.interact ?? ''}|`).join('\n')}\n\n`;
   }
 
   if (addedStories.length === 0) {
@@ -128,7 +128,7 @@ ${differingStories.map(storyInfo => `|${storyInfo.title}|${storyInfo.variantTitl
     markup += `⚠️${addedStories.length} stories/variants were added.
 |Story Title|Variant Title|Story Path|Dark Mode|Writing Mode|Interaction|
 |-----------|-------------|----------|---------|------------|-----------|
-${addedStories.map(storyInfo => `|${storyInfo.title}|${storyInfo.variantTitle}|${storyInfo.relativePath}|${storyInfo.darkMode}|${storyInfo.writingMode ?? ''}|${storyInfo.interact ?? ''}|`).join('\n')}\n\n`;
+${addedStories.map(storyInfo => `|${storyInfo.title}|${storyInfo.variantTitle}|${storyInfo.relativePath}|${storyInfo.darkMode || ''}|${storyInfo.writingMode ?? ''}|${storyInfo.interact ?? ''}|`).join('\n')}\n\n`;
   }
 
   if (dissappearedImages.length === 0) {
