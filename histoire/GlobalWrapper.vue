dbc52dfd8aab07a0ec5c7a681a012ed3b0ddb7b6
adzukimame <88547072+adzuki02@users.noreply.github.com>
2025-10-23 00:14:00 +0900

    minor fix

--- histoire/GlobalWrapper.vue
+++ histoire/GlobalWrapper.vue
@@ -1,7 +1,7 @@
 <template>
   <Html
     :class="[{ 'dark-mode': darkMode }]"
-    :data-writing-mode="verticalLayout ? 'vertical-rl' : null" />
+    :data-writing-mode="verticalLayout ? 'vertical-rl' : 'horizontal-tb'" />
   <div class="outer">
     <div class="inner">
       <slot />
