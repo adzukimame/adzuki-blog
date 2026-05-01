60fece3e4915b12311fea8f8adaa76ea0261b417
adzukimame <88547072+adzukimame@users.noreply.github.com>
2026-05-01 14:29:37 +0000

    productionで目次が表示されるように

--- app/components/ArticleRenderer.vue
+++ app/components/ArticleRenderer.vue
@@ -3,7 +3,8 @@
     v-if="content"
     :value="content"
     tag="article"
-    :class="$style.articleRenderer" />
+    :class="$style.articleRenderer"
+    :components="contentComponents" />
   <div v-else>
     <h2>コンテンツはありません</h2>
   </div>
@@ -11,10 +12,17 @@
 
 <script setup lang="ts">
 import type { PageCollectionItemBase } from '@nuxt/content';
+import ArticleToc from './content/ArticleToc.vue';
+import ImageViewer from './content/ImageViewer.vue';
 
 defineProps<{
   content: PageCollectionItemBase | undefined;
 }>();
+
+const contentComponents = {
+  'article-toc': ArticleToc,
+  'image-viewer': ImageViewer,
+};
 </script>
 
 <style module>
