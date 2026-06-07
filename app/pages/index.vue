8962ad83277644eb85a28ae6f60b167aaeed6f5e
adzukimame <88547072+adzukimame@users.noreply.github.com>
2026-06-07 03:24:21 +0000

    pages/をtailwind化

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div :class="$style.categoryName">
+    <div class="text-[1.2rem] pbe-4 border-be-2 border-split mbe-20">
       すべての投稿
     </div>
     <main ref="articleListOuter">
@@ -11,7 +11,7 @@
       :items-length="articleCount ?? 0"
       :items-per-page="ARTICLE_PER_PAGE"
       page-query-param="p"
-      :class="$style.navigator" />
+      class="my-20" />
   </div>
 </template>
 
@@ -106,16 +106,3 @@ useSeoMeta({
   description: runtimeConfig.public.siteDescription,
 });
 </script>
-
-<style module>
-.categoryName {
-  font-size: 1.2rem;
-  padding-block-end: 0.4rem;
-  border-block-end: solid 2px var(--split);
-  margin-block-end: 2rem;
-}
-
-.navigator {
-  margin-block: 2rem;
-}
-</style>
